563ddc9deb041ec020aa7e1ea5f3a425760c28b3
PapaHero <longwawa@yahoo.de>
2022-03-27 22:13:08 +0200

    Update GroßeMutter

--- Rotkäppchen_Und_Der_Wolf/src/Rotkäppchen/Großmutter.java
+++ Rotkäppchen_Und_Der_Wolf/src/Rotkäppchen/Großmutter.java
@@ -1,5 +1,23 @@
 package Rotkäppchen;
 
+import basis.Farbe;
+import basis.Fenster;
+import basis.Hilfe;
+import basis.Stift;
+
 public class Großmutter {
 
+	Fenster kenntBildschirm;
+	Stift hatStift;
+	
+	public Großmutter(Fenster pBildschirm) {
+		this.kenntBildschirm = pBildschirm;
+		hatStift = new Stift();
+		hatStift.bewegeBis(Hilfe.zufall(10, kenntBildschirm.breite() - 50),
+				Hilfe.zufall(10, kenntBildschirm.hoehe() - 50));
+		// Startpunkt für Hause
+		hatStift.runter();
+		hatStift.setzeFarbe(Farbe.ROT); // male rot Häuschen		
+		
+	}
 }
