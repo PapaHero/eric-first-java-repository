e9597990289c4fcdbf13bfc5d1487b93d9fd7ffe
PapaHero <longwawa@yahoo.de>
2022-04-02 15:16:45 +0200

    Add Javadoc Kommentar

--- Rotkäppchen_Und_Der_Wolf/src/Rotkäppchen/Wolf.java
+++ Rotkäppchen_Und_Der_Wolf/src/Rotkäppchen/Wolf.java
@@ -4,14 +4,21 @@ import basis.*;
 
 public class Wolf extends Waldbewohner {
 
-	// Konstruktor
+	/** 
+	* Klasse Konstruktor erweitertet der Waldbewohner. Setz Farbe der Stift auf Schwarz für Klasse Wolf 
+	* @param pGeschw Geschwindigkeit wie schnell der Wolf pro Zeiteinheit bewegen kann
+	* @param pBildschirm Oberfläche für Wolf
+	* @see Waldbewohner   
+	*/
+	
 	public Wolf(double pGeschw, Fenster pBildschirm) {
 		super(pGeschw, pBildschirm);
 		hatStift.setzeFarbe(Farbe.SCHWARZ);
 	}
 
+	
+	
 	public void bewege() {
-		
 		
 		hatStift.dreheInRichtung(xR, yR); // dreht sich in Rotkäppchens-Richtung
 		vermeidenHindernis();
--- Rotkäppchen_Und_Der_Wolf/src/Rotkäppchen/start.java
+++ Rotkäppchen_Und_Der_Wolf/src/Rotkäppchen/start.java
@@ -1,14 +1,16 @@
 package Rotkäppchen;
+
 public class start {
 
 	/**
+	 * Startprogramm für Rotkäppchen Package
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-MeinProgramm p;
-p= new MeinProgramm(); //P wird erstellt
-p.fuehreAus();  //P wird ausgeführt
+		MeinProgramm p;
+		p = new MeinProgramm(); // P wird erstellt
+		p.fuehreAus(); // P wird ausgeführt
 	}
 
 }
